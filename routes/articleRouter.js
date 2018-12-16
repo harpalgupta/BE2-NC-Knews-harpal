046f17d3711e45a4ca0ecc8efcdaa2261290f565
Harpal Gupta <harpal.gupta@gmail.com>
2018-12-16 17:08:23 +0000

    check for incorrect comment id

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,23 +1,49 @@
 const articleRouter = require('express').Router();
+const { handle405 } = require('../errors');
 const {
-  handle405,
-} = require('../errors');
-const {
-  getArticles, updateArticle, deleteArticle, getCommentsForArticle,
-  addCommentForArticle, updateComment, deleteComment,
+  getArticles,
+  updateArticle,
+  deleteArticle,
+  getCommentsForArticle,
+  addCommentForArticle,
+  updateComment,
+  deleteComment,
 } = require('../controllers/articleController');
 
 const checkArticleID = (req, res, next, articleid) => {
   // console.log('checking', articleid);
-  if (articleid.match(/\D/)) { return next({ status: 400, msg: 'Article ID must be integer' }); }
+  if (articleid.match(/\D/)) {
+    return next({ status: 400, msg: 'Article ID must be integer' });
+  }
+  return next();
+};
+const checkCommentID = (req, res, next, commentid) => {
+  // console.log('checking', articleid);
+  if (commentid.match(/\D/)) {
+    return next({ status: 400, msg: 'Comment ID must be integer' });
+  }
   return next();
 };
 // GET /api/topics/:topic/articles
 articleRouter.get('/', getArticles).all(handle405);
 articleRouter.param('article_id', checkArticleID);
-articleRouter.route('/:article_id/comments/:comment').patch(updateComment).delete(deleteComment).all(handle405);
-articleRouter.route('/:article_id/comments').get(getCommentsForArticle).post(addCommentForArticle).all(handle405);
-articleRouter.route('/:article_id').get(getArticles).patch(updateArticle).delete(deleteArticle);
+articleRouter.param('comment', checkCommentID);
+
+articleRouter
+  .route('/:article_id/comments/:comment')
+  .patch(updateComment)
+  .delete(deleteComment)
+  .all(handle405);
+articleRouter
+  .route('/:article_id/comments')
+  .get(getCommentsForArticle)
+  .post(addCommentForArticle)
+  .all(handle405);
+articleRouter
+  .route('/:article_id')
+  .get(getArticles)
+  .patch(updateArticle)
+  .delete(deleteArticle);
 
 // articleRouter.post('/api/articles/:article_id/comments', addCommentsForArticle);
 
