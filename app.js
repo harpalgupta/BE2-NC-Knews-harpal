4784dcd7e5f2d95ce30f336eaad541b9e7080ec6
Harpal Gupta <harpal.gupta@ensek.co.uk>
2019-08-15 14:12:07 +0100

    fix un-needed host info for swagger

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const swaggerDoc = require('./swagger.json');
 const apiRouter = require('./routes/apiRouter');
 const { handle404, handleOtherErrors, handle422 } = require('./errors');
 
-const { PORT = '9090', HOST = 'localhost' } = process.env;// v1 api routes
+// const { PORT = '9090', HOST = 'localhost' } = process.env;// v1 api routes
 
 
-swaggerDoc.host = `${HOST}:${PORT}`;
+// swaggerDoc.host = `${HOST}:${PORT}`;
 
 // app.use('/api/v1/', require('./routes/'));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
