de95e05b2a1447252e58f2a342fb470c32cfd961
Harpal Gupta <harpal.gupta@ensek.co.uk>
2019-08-15 13:49:54 +0100

    add config to populate production with dev data

--- db/data/index.js
+++ db/data/index.js
@@ -1,9 +1,10 @@
 const test = require('./test-data');
 const development = require('./development-data');
+const production = require('./development-data');
 
 const env = process.env.NODE_ENV || 'development';
 
 
-const data = { test, development };
+const data = { test, development,production };
 
 module.exports = data[env];
