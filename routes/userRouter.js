616c3d1a3db7d657771358a907ad5f2bcce9e67e
Harpal Gupta <harpal.gupta@gmail.com>
2021-07-04 18:46:18 +0100

    revert user controller issue, try setting node v12 for heroku knex

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,5 @@
 const userRouter = require('express').Router();
 const { getUsers } = require('../controllers/usersController');
-const { getApiDescription } = require('../controllers/apiController');
-
 const {
   handle405,
 } = require('../errors');
@@ -12,7 +10,7 @@ const {
  *    get:
  *      description: This should return all users
  */
-userRouter.get('/', getApiDescription).all(handle405);
+userRouter.get('/', getUsers).all(handle405);
 
 userRouter.get('/:username', getUsers).all(handle405);
 
